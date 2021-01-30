bcbf7899e80b22bb65722132b8bfca2568a4f02a
Arthur Sharipov <ar7hua@gmail.com>
2021-01-30 17:51:26 +0300

    upd test

--- test/app_test.clj
+++ test/app_test.clj
@@ -1,11 +1,26 @@
 (ns app-test
   (:require [app :refer [app]]
-            [db.core :refer [db]]
+            [db.core :as conn]
             [clojure.java.jdbc :as jdbc]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [spec :as spec]
             [clojure.test :refer :all]))
+
+(def db* (assoc conn/db :dbname "db_test"))
+
+(defn create-patients-table [] ((jdbc/execute! db* "drop table if exists ")
+                                (->> (io/resource "sql/create.sql")
+                                     io/file
+                                     slurp
+                                     (jdbc/execute! db*))))
+
+(defn recreate-patients-table []
+  (let [query (-> (io/resource "sql/create.sql")
+                  io/file slurp
+                  (clojure.string/replace #"\n" " "))]
+    (jdbc/execute! db* "drop table if exists patients")
+    (jdbc/execute! db* query)))
 
 (defn fix-insert-data [t] (let [patients (->> (io/resource "seeds/patients.edn")
                                               io/file
@@ -14,8 +29,8 @@
                                               :data
                                               (map spec/confrom)
                                               (map #(dissoc % :id)))]
-                            (jdbc/execute! db "truncate patients cascade;")
-                            (jdbc/insert-multi! db :patients patients)
+                            (recreate-patients-table)
+                            (jdbc/insert-multi! db* :patients patients)
                             (t)))
 
 (use-fixtures :each fix-insert-data)
@@ -40,5 +55,5 @@
 
 (comment
   (fix-insert-data)
-  (jdbc/execute! db "truncate patients cascade;")
-  (jdbc/execute! db "select count(*) from patients"))
+  (jdbc/execute! db* "truncate patients cascade;")
+  (jdbc/execute! db* "select count(*) from patients"))
