11a205992f017e4258967357bc5248a3546a7bfd
Arthur Sharipov <ar7hua@gmail.com>
2021-02-15 22:18:48 +0300

    codeformat

--- src/hs/back/db/core.clj
+++ src/hs/back/db/core.clj
@@ -1,12 +1,5 @@
 (ns hs.back.db.core)
 
-;; (def db  {:dbtype "postgresql"
-;;           :dbname "db_dev"
-;;           :host "localhost"
-;;           :user "postgres"
-;;           :password "mysecretpassword"
-;;           :port 5432})
-
 (defn connection [ctx]
   (let [cfg (get-in ctx [:config :db])]
     {:dbtype "postgresql"
